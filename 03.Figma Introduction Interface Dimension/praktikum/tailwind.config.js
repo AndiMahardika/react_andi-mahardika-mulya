92c389529654c7736fb5b30045ece477d8df24d2
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-09-25 22:11:08 -0700

    add: landing page pixel perfect

---
+++ 03.Figma Introduction Interface Dimension/praktikum/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        jost: ['Jost', 'sans-serif'], // Tambahkan font Jost
+        roboto: ['Roboto', 'sans-serif'], // Tambahkan font Roboto
+      },
+    },
+  },
+  plugins: [],
+}
