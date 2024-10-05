a561b12190b7f08c8c4b8437bc0cd647a5a411bb
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-10-04 22:38:59 -0700

    add: assign event handling

---
+++ 13.Even Handling/praktikum/src/pages/createProduct/components/index.jsx
@@ -0,0 +1,119 @@
+import Input from "../../../components/input/input";
+import Navbar from "../../../components/navbar/navbar";
+import Select from "../../../components/select/select";
+import Radio from "../../../components/radio/radio";
+import Textarea from "../../../components/textarea/textarea";
+import Button from "../../../components/button/button";
+import ListProduct from "./listProduct";
+import { useState } from "react";
+import Hero from "../../../components/hero";
+import article from "./article";
+
+const freshness = [{
+    label: "Brand New",
+    value: "brand-new"
+  },
+  {
+    label: "Second Hank",
+    value: "second-hank"
+  },
+  {
+    label: "Refurbished",
+    value: "refurbished"
+  }
+]
+
+const productCategory = ["clothes", "electronic", "food"]
+export default function CreateProduct() {
+  const [products, setProducts] = useState([])
+  const [productName, setProductName] = useState("")
+  const [language, setLanguage] = useState("en")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+
+    const newProduct = {
+      id: generateId(),
+      name: formData.get("productName"),
+      category: formData.get("productCategory"),
+      image: formData.get("imageProduct"),
+      freshness: formData.get("productFreshness"),
+      description: formData.get("description"),
+      price: formData.get("productPrice")
+    }
+
+    setProducts([...products, newProduct])
+  }
+
+  function generateId() {
+    return new Date().getTime()
+  }
+
+  function randomNumber() {
+    console.log(Math.random())
+  }
+
+  const handleValidationProductName = (e) => {
+    const productNameValue = e.target.value;
+    setProductName(productNameValue);
+
+    if(!productNameValue) {
+      alert("Please enter a valid product name.")
+    }
+
+    if(productNameValue.length > 25) {
+      alert("Last Name must not exceed 25 characters.")
+    }
+
+    if(productNameValue.length > 10) {
+      alert("Last Name must not exceed 10 characters.")
+    }
+  }
+
+  const handleChangeText = () => {
+    setLanguage(language === "en" ? "id" : "en")
+  }
+  
+  return (
+    <>
+      <Navbar/>
+      <main>
+        <div className="container">
+          <Hero article={article} language={language} />
+
+          <div className="mx-auto text-center d-flex justify-content-center gap-3">
+            <Button type="button" color="primary" onClick={randomNumber} >Random Number</Button>
+            <Button type="button" color="primary" onClick={handleChangeText} >change text</Button>
+          </div>
+
+          <form onSubmit={handleSubmit} className="row needs-validation" id="createProductForm" encType="multipart/form-data">
+            <div className="col-lg-7 mx-auto">
+              <h3>Detail Product</h3>
+              <Input label="Product Name" placeholder="Product Name" type="text" id="productName" col="6" onChange={handleValidationProductName} />
+              <Select caption="Product Category" options={productCategory} id="productCategory" />
+              <div className="mb-4 col-md-4 col-7">
+                <label htmlFor="imageProduct" className="form-label">Image Of Product</label>
+                <input className="form-control form-control-sm" type="file" id="imageProduct" />
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+                <div className="invalid-feedback">
+                  Please select a image.
+                </div>
+              </div>
+              <Radio label="Product Freshness" options={freshness} name="productFreshness" />
+              <Textarea label="Additional Description" placeholder="Additional Description" id="description" row="3" />
+              <Input label="Product Price" placeholder="$ 1" type="number" id="productPrice" col="12" />
+              <div className="d-grid gap-2 px-4 mb-3">
+                <Button type="submit" color="primary">Submit</Button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <ListProduct dataProducts={products} />
+      </main>
+    </>
+  )
+}
---
+++ 13.Even Handling/praktikum/src/pages/createProduct/components/listProduct.jsx
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react";
+
+export default function ListProduct(props) {
+  const { dataProducts } = props;
+  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState({});
+  const [search, setSearch] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    setProducts(dataProducts);
+  }, [dataProducts]);
+
+  function deleteProduct() {
+    products.pop()
+    const newProducts = [...products]
+    setProducts(newProducts)
+  }
+
+  function searchProduct() {
+    setShowAlert(true)
+    if(search === "") {
+      setProduct(null)
+    } else {
+      const foundProduct = products.find(product => product.name.toLowerCase() === search.toLowerCase());
+      setProduct(foundProduct)
+    }
+  }
+  
+  return (
+    <div className="container-fluid">
+      {showAlert && (
+        <div className="row px-2" id="searchAlert">
+        {product ? (
+          <div className="alert alert-success alert-dismissible fade show col-md-9 col-lg-5 col-10 mx-auto" role="alert">
+          <h5 className="text-center">Product Found</h5>
+          <hr />
+          <div className="d-md-flex align-items-center justify-content-evenly text-center">
+            <div>
+              <strong>Product Name:</strong> {product.name} <br />
+              <strong>Product Category:</strong> {product.category} <br />
+              <strong>Product Freshness:</strong> {product.freshness} <br />
+              <strong>Product Price:</strong> $ {product.price} <br />
+            </div>
+            <img src="{product.image}" className="rounded-3 mt-2 mt-md-0" alt="Product Image" width="130px" />
+          </div>
+          <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        ) : (
+          <div className="alert alert-danger alert-dismissible fade show col-md-9 col-lg-6 col-12 mx-auto" role="alert">
+            <strong>Product not found</strong>
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+        )}
+      </div>
+      )}
+
+      <h2 className="text-center">List of Products</h2>
+      <div className="row overflow-auto mb-2">
+        <div className="col-12">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Product Category</th>
+                <th scope="col">Image of Product</th>
+                <th scope="col">Product Freshness</th>
+                <th scope="col">Additional Description</th>
+                <th scope="col">Product Price</th>
+              </tr>
+            </thead>
+            <tbody id="tableProduct">
+              {products && products.map((product, index) => (
+                <tr className="align-middle" key={product.id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{product.name}</td>
+                  <td>{product.category}</td>
+                  <td>
+                    <img src={product.image} className="img-fluid rounded-3" width="130px" alt={product.image} />
+                  </td>
+                  <td>{product.freshness}</td>
+                  <td>{product.description}</td>
+                  <td>${product.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-2 col-md-4 col-7">
+          <input 
+            type="text" 
+            className="form-control form-control-sm" 
+            placeholder="Search by Product Name" 
+            id="inputSearch" 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <div className="btn-group pb-5" role="group" aria-label="Basic example">
+            <button 
+              type="button" 
+              id="btnDelete" 
+              className="btn btn-danger btn-sm" 
+              data-bs-toggle="tooltip" 
+              data-bs-placement="right" 
+              title="Delete last Product"
+              onClick={deleteProduct}
+            >
+              Deletion
+            </button>
+            <button 
+              type="button" 
+              className="btn btn-outline-primary btn-sm" 
+              id="btnSearch"
+              onClick={searchProduct}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
