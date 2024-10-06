9a7b3c7b3db06510ad365d02cbc765abf97597d1
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-10-05 21:10:05 -0700

    add: assign react hook

---
+++ 14.React Hook/praktikum/src/components/input/input.jsx
@@ -0,0 +1,18 @@
+export default function Input(props) {
+  const {label, placeholder, type, id, col, onChange = () => {} } = props
+
+  return (
+    <>
+      <div className={`mb-4 col-md-${col} col-7`}>
+        <label htmlFor={id} className="form-label">{label}</label>
+        <input type={type} className="form-control" id={id} placeholder={placeholder} name={id} onChange={onChange} autoComplete="off" required/>
+        <div className="valid-feedback">
+          Looks good!
+        </div>
+        <div className="invalid-feedback">
+          Please fill out this field.
+        </div>
+      </div>
+    </>
+  )
+}
